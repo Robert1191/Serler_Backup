238d9f60350ef5007ba1eabc1a9f0fb447afe9e1
Athena <hit815@163.com>
2016-09-05 17:23:06 +1200

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   resources :users
   root 'home#index'
 
+  get    '/login',   to: 'sessions#create'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
